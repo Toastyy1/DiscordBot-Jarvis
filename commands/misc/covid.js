cc1c748095adff4fad72ddb28b57d1c57cf79390
Toasty <marcel_neber@gmx.de>
2021-03-25 01:09:11 +0100

    Improved covidTracker command
    [WIP]

--- commands/misc/covid.js
+++ commands/misc/covid.js
@@ -2,13 +2,16 @@ const fetch = require('node-fetch');
 
 
 module.exports = {
-    name: 'covid',
-    execute: async (messgae, args, Discord) => {
-        
-        fetch('https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
-            .then(res => res.json())
-            .then(data => data.features.foreach(x => console.log(x)))
-            .catch(error => console.log('Error!'));
+	name: 'covid',
+	execute: async (messgae, args, Discord) => {
 
-    }
-}
+		fetch('https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+			.then(res => res.json())
+			.then(data => {
+				const { features } = data;
+				console.log(features.filter(filter => filter.attributes.Bundesland == 'Baden-Württemberg' && filter.attributes.Landkreis == 'LK Schwäbisch Hall'));
+			})
+			.catch(error => console.log('Error!' + error));
+
+	},
+};
