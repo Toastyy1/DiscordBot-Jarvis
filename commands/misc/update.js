a82ad96821db06b8ec764a57a132c8a83bb07e25
Marcel <marcel_neber@gmx.de>
2021-03-27 01:40:16 +0100

    Small improvements
    //WIP

--- commands/misc/update.js
+++ commands/misc/update.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = path.dirname(require.main.filename);
 
 module.exports = {
 	name: 'update',
 	execute: (message, args, Discord) => {
-        // Call changelog here
+		const changelogPath = `${rootDir}/CHANGELOG.md`;
+
+		const data = fs.readFileSync(changelogPath, 'utf-8');
+		console.log(data.toString());
 	},
 };
