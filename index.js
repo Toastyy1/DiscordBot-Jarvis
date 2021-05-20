76fd242aee5ef6a3f4795b583a41324171cd1685
Marcel <marcel_neber@gmx.de>
2021-05-21 01:53:05 +0200

    Implemented the auto role assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
