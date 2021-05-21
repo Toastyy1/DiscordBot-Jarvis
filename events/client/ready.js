9024d70c78835df4291bbeccc1b754fd7a140be3
Marcel <marcel_neber@gmx.de>
2021-05-21 03:24:46 +0200

    Changed the bot's activity

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,5 @@
 module.exports = client => {
 	const guildCount = client.guilds.cache.size > 1 ? `over ${client.guilds.cache.size} servers` : `over ${client.guilds.cache.size} server`;
-	client.user.setActivity(guildCount, { type: 'WATCHING' });
+	client.user.setActivity(`Prefix = '${process.env.PREFIX}'`, { type: 'WATCHING' });
 	console.log('Bot is online!');
 };
