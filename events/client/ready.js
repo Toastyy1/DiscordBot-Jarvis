4510264af2d9582339d0cfe32014eeeb8e869185
Marcel <marcel_neber@gmx.de>
2021-05-26 18:38:55 +0200

    Added database connection

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,18 @@
-module.exports = client => {
+const mongo = require('../../util/mongo')
+
+module.exports = async  client => {
 	const guildCount = client.guilds.cache.size > 1 ? `over ${client.guilds.cache.size} servers` : `over ${client.guilds.cache.size} server`;
 	client.user.setActivity(`Prefix = '${process.env.PREFIX}'`, { type: 'WATCHING' });
+	
+	await mongo().then(mongoose => {
+		try {
+			console.log('Successfully connected to database!')
+		} catch (error) {
+			console.log('An error has occured while connecting to the database:\n' + error)
+		} finally {
+			mongoose.connection.close();
+		}
+	})
+	
 	console.log('Bot is online!');
 };
---
+++ util/mongo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const mongoPath = `mongodb+srv://admin:${process.env.MONGODBPASSWORD}@cluster0.vetul.mongodb.net/${process.env.DATABASENAME}?retryWrites=true&w=majority`;
+
+module.exports = async () => {
+    await mongoose.connect(mongoPath, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).catch(err => console.log('An error has occured while connecting to the database:\n' + err));
+
+    return mongoose;
+};
