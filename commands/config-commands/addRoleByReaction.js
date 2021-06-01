e35e498b9b79887670d84b1f9c4cb06823fa1515
Marcel <marcel_neber@gmx.de>
2021-06-01 22:53:52 +0200

    Improved 'reactionRole' command
    Accessing the database has been reduced

--- commands/config-commands/addRoleByReaction.js
+++ commands/config-commands/addRoleByReaction.js
@@ -3,59 +3,49 @@ const messageReactionSchema = require('../../schemas/messageReaction-schema');
 const { cache } = require('../../events/client/messageReactionAdd');
 
 module.exports = {
-	name: 'addrolebyreaction',
+	name: 'reactionRole',
+	aliases: [ 'rr' ],
 	guildOnly: true,
-	minArgs: 3,
+	minArgs: 2,
 	maxArgs: 3,
 	expectedArgs: '<target message (ID)> <target reaction> <role (ID) you want to assign>',
 	permissions: ['ADMINISTRATOR'],
 	execute: async (message, args) => {
-		const { guild } = message;
 		const targetMessage = args[0];
 		const targetMessageObj = await message.channel.messages.fetch(targetMessage);
 		const targetReaction = args[1];
 		const role = args[2];
 
-		if (targetMessageObj.reactions.cache.find(msgReaction => msgReaction.emoji.name === targetReaction.split(':')[1])) return message.channel.send('Reaction already exists!');
-
-		await targetMessageObj.react(targetReaction);
-
 		await mongo().then(async mongoose => {
 			try {
-				await messageReactionSchema.findOneAndUpdate({
-					_id: guild.id,
-					message: {
-						$elemMatch: {
-							msgId: targetMessage,
-						},
-					},
-				}, {
-					_id: guild.id,
-					$push: {
-						message: [{
-							msgId: targetMessage,
-							$each: {
+				await messageReactionSchema.findById(targetMessage).then(async doc => {
+					if(doc) {
+						if(targetMessageObj.reactions.cache.find(msgReaction => msgReaction.emoji.name === targetReaction.split(':')[1])) {
+							const existingReaction = doc.reactionRole.find(obj => obj.reaction === targetReaction).role;
+
+							if(existingReaction === role) return message.reply('That reaction & role for the entered message already exists!');
+						}
+						await doc.update({
+							_id: targetMessage,
+							$push: {
 								reactionRole: [{
 									reaction: targetReaction,
 									role,
 								}],
 							},
-						}],
-					},
+						});
+					}
+					else {
+						await messageReactionSchema.create({
+							_id: targetMessage,
+							reactionRole: [{
+								reaction: targetReaction,
+								role,
+							}],
+						});
+					}
 
-				}, {
-					upsert: true,
 				});
-
-				cache[guild.id] = [
-					[{
-						msgId: targetMessage,
-						reactionRole: [{
-							reaction: targetReaction,
-							role,
-						}],
-					}],
-				];
 			}
 			finally {
 				mongoose.connection.close();
