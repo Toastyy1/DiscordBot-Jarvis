fbf5b39b6895e7ee9e941d9e11c2b062b15c1156
Marcel <marcel_neber@gmx.de>
2021-06-22 22:27:29 +0200

    Removed code from 'messageReactionRemove' event

--- events/client/messageReactionRemove.js
+++ events/client/messageReactionRemove.js
@@ -9,39 +9,4 @@ module.exports = async (client, Discord, reaction, user) => {
 
 	if(user.bot) return;
 
-	const member = await reaction.message.guild.members.fetch(user.id);
-	const reactionRoles = [
-		{
-			reaction: 'develop',
-			role: '827332590620246056',
-		},
-
-		{
-			reaction: 'stackoverflow',
-			role: '835926532723703849',
-		},
-
-		{
-			reaction: 'age16',
-			role: '827622099270762507',
-		},
-
-		{
-			reaction: 'age18',
-			role: '827622498689351711',
-		},
-
-		{
-			reaction: 'csgo',
-			role: ['841083180097208340', '841082846973394974'],
-		},
-	];
-
-	if(reaction.message.id === process.env.ROLECLAIMMESSAGE) {
-		member.roles
-			.remove(
-				reactionRoles.find((rr) => rr.reaction === reaction.emoji.name).role,
-			)
-			.catch(console.error());
-	}
 };
