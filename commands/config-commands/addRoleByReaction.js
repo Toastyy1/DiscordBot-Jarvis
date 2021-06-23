ba56df89798e4c1e5c450ae15f738dc1ab8196aa
Marcel <81110115+Toasty65@users.noreply.github.com>
2021-06-23 06:38:56 +0000

    Another small fix

--- commands/config-commands/addRoleByReaction.js
+++ commands/config-commands/addRoleByReaction.js
@@ -22,7 +22,7 @@ module.exports = {
     args[2].startsWith('<@') ? role = args[2].substring(3, args[2].length - 1) : role = args[2];
 
     let isCategoryRole;
-    isCategoryRole && isCategoryRole === 'true'? isCategoryRole = args[3] : isCategoryRole = false;
+    isCategoryRole && args[3] === 'true'? isCategoryRole = true : isCategoryRole = false;
 
     await mongo().then(async mongoose => {
       try {
