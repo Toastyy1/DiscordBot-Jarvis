31005efbe823d8b8054a4f663d4f5886170dc0f0
Marcel <marcel_neber@gmx.de>
2021-06-25 12:20:25 +0200

    Using 'Permissions.FLAGS' instead of defining them manually

--- events/guild/message.js
+++ events/guild/message.js
@@ -5,41 +5,8 @@ const prefixSchema = require('../../schemas/prefix-schema');
 const cache = {};
 
 const validatePermissions = (permissions) => {
-	// const { Permissions } = require('discord.js');
-	// const validPermissions = Permissions;
-	const validPermissions = [
-		'CREATE_INSTANT_INVITE',
-		'KICK_MEMBERS',
-		'BAN_MEMBERS',
-		'ADMINISTRATOR',
-		'MANAGE_CHANNELS',
-		'MANAGE_GUILD',
-		'ADD_REACTIONS',
-		'VIEW_AUDIT_LOG',
-		'PRIORITY_SPEAKER',
-		'STREAM',
-		'VIEW_CHANNEL',
-		'SEND_MESSAGES',
-		'SEND_TTS_MESSAGES',
-		'MANAGE_MESSAGES',
-		'EMBED_LINKS',
-		'ATTACH_FILES',
-		'READ_MESSAGE_HISTORY',
-		'MENTION_EVERYONE',
-		'USE_EXTERNAL_EMOJIS',
-		'VIEW_GUILD_INSIGHTS',
-		'CONNECT',
-		'SPEAK',
-		'MUTE_MEMBERS',
-		'DEAFEN_MEMBERS',
-		'MOVE_MEMBERS',
-		'USE_VAD',
-		'CHANGE_NICKNAME',
-		'MANAGE_NICKNAMES',
-		'MANAGE_ROLES',
-		'MANAGE_WEBHOOKS',
-		'MANAGE_EMOJIS',
-	];
+	const { Permissions } = require('discord.js');
+	const validPermissions = Object.keys(Permissions.FLAGS);
 
 	for (const permission of permissions) {
 		if (!validPermissions.includes(permission)) {
