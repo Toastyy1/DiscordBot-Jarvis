4df788b342c596673609c1dd8cdce003c5690ca9
Marcel <81110115+Toasty65@users.noreply.github.com>
2021-07-01 13:42:36 +0000

    TEMPORARILY SOLUTION: New users are now getting the 'Recruit' role due to current bot issues

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -9,7 +9,10 @@ module.exports = async (client, Discord, member) => {
 	// Adding the 'Roles:' role
 	await member.roles.add('852085441745911838');
 	// Adding the 'Not yet verified' role
-	await member.roles.add('852615800434196501');
+	//await member.roles.add('852615800434196501');
+
+  // Adding the 'Recruit' role (TEMPORARILY SOLUTION)
+  await member.roles.add('852065782317580339');
 
 	let data = cache[guild.id];
 
